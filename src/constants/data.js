ceeb7276ab20d27ec20772d54945ada6a66f98aa
Zerubabel <zerubabelyon@gmail.com>
2024-06-10 06:37:51 -0700

    initial file and data setup

---
+++ src/constants/data.js
@@ -0,0 +1,111 @@
+import {amazon__gadget, camera, chair, cleats, cpu_cooler, desktop, dog_food, gucci_bag,
+         gucci_perfume, iphone, jacket, joystick_2, joystick, keyboard, laptop, ps5,
+         ps5_1, ps5_2, ps5_3, ps5_4, rain_coat, shelf, speaker, toy_car, woman
+        } from '../assets';
+
+export const navLinks = [
+    {
+        id: 'home',
+        title: 'Home'
+    },
+    {
+        id: 'about',
+        title: 'About'
+    },
+    {
+        id: 'sign up',
+        title: 'Sign Up'
+    },
+    {
+        id: 'contact',
+        title: 'Contact'
+    }
+]
+
+export const categories = [
+    {
+        id: 'women fashion',
+        title: 'Women\'s Fashion'
+    },
+    {
+        id: 'men fashion',
+        title: 'Men\'s Fashion' 
+    },
+    {
+        id: 'electronics',
+        title: 'Electronics' 
+    },
+    {
+        id: 'sports',
+        title: 'Sports' 
+    },
+    {
+        id: 'baby\'s and toys',
+        title: 'Baby\'s and Toys' 
+    },
+]
+
+export const flashSaleProducts = [
+    {
+        id: 1,
+        title: "HAVIT HV-G92 Gamepad",
+        regularPrice: 160,
+        salePrice: 120,
+        img: joystick_2
+    },
+    {
+        id: 2,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    {
+        id: 3,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    {
+        id: 4,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    {
+        id: 5,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    {
+        id: 6,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    {
+        id: 7,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    {
+        id: 8,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    {
+        id: 9,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    {
+        id: 10,
+        title: "AK-900 Wired Keyboard",
+        regularPrice: 1160,
+        salePrice: 960,
+    },
+    
+]
